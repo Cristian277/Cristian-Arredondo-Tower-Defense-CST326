f80f8c052f7e93447f63563923fc3fd522a3dfe4
Cristian <carredondo@csumb.edu>
2021-03-21 11:14:00 -0700

    First Commit

---
+++ Tower Defense Project/Assets/Tower Defense/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name == "BigBadGuy")
+                {
+                    Debug.Log("Big Bad guy Hit");
+                }
+                else if (hit.collider.gameObject.name == "SmallBadGuy")
+                {
+                    Debug.Log("Small Bad guy Hit");
+                }
+            }
+        }
+    }
+}
